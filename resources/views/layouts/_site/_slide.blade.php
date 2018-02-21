d02b651b844b412207b7a7a9c011c21b2896c4e6
Edivã Goncalves Teodozio <ediva.teodozio@gestao.pms.ad>
2018-02-21 17:04:24 -0300

    Criação de slide

---
+++ resources/views/layouts/_site/_slide.blade.php
@@ -0,0 +1,32 @@
+<div class="slider">
+  <ul class="slides">
+   <li>
+      <img src="{{asset('img/modelo_slide_1.jpg')}}" alt="">
+        <div class="caption center-align">
+            <h3>Titulo imange</h3>
+            <h5>descrição</h5>
+        </div>
+   </li> 
+   <li>
+      <img src="{{asset('img/modelo_slide_2.jpg')}}" alt="">
+        <div class="caption left-align">
+            <h3>Titulo imange</h3>
+            <h5>descrição</h5>
+        </div>
+   </li>
+   <li>
+      <img src="{{asset('img/modelo_slide_2.jpg')}}" alt="">
+        <div class="caption right-align">
+            <h3>Titulo imange</h3>
+            <h5>descrição</h5>
+        </div>
+   </li>
+   <li>
+      <img src="{{asset('img/modelo_slide_4.jpg')}}" alt="">
+        <div class="caption center-align">
+            <h3>Titulo imange</h3>
+            <h5>descrição</h5>
+        </div>
+   </li>
+ </ul>
+<div>
