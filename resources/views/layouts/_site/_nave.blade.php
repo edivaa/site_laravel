e8b5ff3b1f1549a99cad1fcfb08e9dc33cb139a7
Edivã Goncalves Teodozio <ediva.teodozio@gestao.pms.ad>
2018-02-21 17:20:07 -0300

    Ajuste nave bar

--- resources/views/layouts/_site/_nave.blade.php
+++ resources/views/layouts/_site/_nave.blade.php
@@ -3,17 +3,16 @@
              <div class="container">  
                 <a href="#!" class="brand-logo">Sistema de Monitoramento de Gasto</a>
                 <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-               <ul class="right hide-on-med-and-down">
-               <ul class="right hide-on-med-and-down">
+                <ul class="right hide-on-med-and-down">
+                <ul class="right hide-on-med-and-down">
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Sobre</a></li>
+                  <li><a href="#">Contato</a></li>
+                </ul>
+                <ul class="side-nav" id="mobile-demo">
                   <li><a href="#">Home</a></li>
                  <li><a href="#">Sobre</a></li>
                  <li><a href="#">Contato</a></li>
-              </ul>
-              <ul class="side-nav" id="mobile-demo">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Sobre</a></li>
-                <li><a href="#">Contato</a></li>
-               
               </ul>
 
                  </div>
