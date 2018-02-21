6008a3ca30be761d289add3852dabec80b312d8a
Edivã Goncalves Teodozio <ediva.teodozio@gestao.pms.ad>
2018-02-21 17:20:41 -0300

    Ajuste slide

--- resources/views/layouts/_site/_slide.blade.php
+++ resources/views/layouts/_site/_slide.blade.php
@@ -15,15 +15,8 @@
         </div>
    </li>
    <li>
-      <img src="{{asset('img/modelo_slide_2.jpg')}}" alt="">
+      <img src="{{asset('img/modelo_slide_3.jpg')}}" alt="">
         <div class="caption right-align">
-            <h3>Titulo imange</h3>
-            <h5>descrição</h5>
-        </div>
-   </li>
-   <li>
-      <img src="{{asset('img/modelo_slide_4.jpg')}}" alt="">
-        <div class="caption center-align">
             <h3>Titulo imange</h3>
             <h5>descrição</h5>
         </div>
