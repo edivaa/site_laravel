887ad4c0e4ad51e0948560d4d1984a017b1ddcd3
Edivã Goncalves Teodozio <ediva.teodozio@gestao.pms.ad>
2018-02-22 16:39:08 -0300

    Criacao de novo item menu

--- resources/views/layouts/_site/_nave.blade.php
+++ resources/views/layouts/_site/_nave.blade.php
@@ -5,13 +5,15 @@
                 <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
                 <ul class="right hide-on-med-and-down">
                 <ul class="right hide-on-med-and-down">
-                  <li><a href="#">Home</a></li>
+                  <li><a href="\">Home</a></li>
                   <li><a href="#">Sobre</a></li>
+                  <li><a href="arquivo/uploadView">Upload file</a></li>
                   <li><a href="#">Contato</a></li>
                 </ul>
                 <ul class="side-nav" id="mobile-demo">
                   <li><a href="#">Home</a></li>
                  <li><a href="#">Sobre</a></li>
+                 <li><a href="upload">Upload file</a></li>
                  <li><a href="#">Contato</a></li>
               </ul>
 
